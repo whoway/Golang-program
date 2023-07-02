bbc7af89589749c99ee634a53da9f7d29aac97a0
whoway <whoway2017@qq.com>
2023-07-02 22:50:13 +0800

    [code] 使用go.mod自动帮助我们导包+使用go.sum来校验完整性

---
+++ 06.modules_test/go.mod
@@ -0,0 +1,8 @@
+module github.com/whoway/test
+
+go 1.20
+
+require (
+	github.com/aceld/zinx v1.1.21 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
---
+++ 06.modules_test/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aceld/zinx/ziface"
+	"github.com/aceld/zinx/znet"
+)
+
+// PingRouter MsgId=1的路由
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+// Ping Handle MsgId=1的路由处理方法
+func (r *PingRouter) Handle(request ziface.IRequest) {
+	//读取客户端的数据
+	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
+}
+
+func main() {
+	//1 创建一个server服务
+	s := znet.NewServer()
+
+	//2 配置路由
+	s.AddRouter(1, &PingRouter{})
+
+	//3 启动服务
+	s.Serve()
+}
